ca572d5c8b54071155fa874dcbfb738d090bb3af
crishe07 <124082370+crishe07@users.noreply.github.com>
2023-02-28 18:03:39 +0100

    Add files via upload
    Ultimas correcciones de lista resultado: manager

--- p1.py
+++ p1.py
@@ -7,9 +7,9 @@ Created on Thu Feb 23 09:14:09 2023
 """
 
 from multiprocessing import Process
-from multiprocessing import BoundedSemaphore, Semaphore, Lock
+from multiprocessing import Semaphore, Lock
 from multiprocessing import current_process
-from multiprocessing import Value
+from multiprocessing import Value, Manager
 import random
 
 K=10
@@ -20,29 +20,30 @@ def dar_numero(lvalue,semaf):
     if lvalue.value !=-1: #es trampa?
         semaf.acquire()
         if lvalue.value==-2:
-            n=random.randint(1,10)
+            lvalue.value=random.randint(0,5) #inicilización
         else:
-            n=random.randint(lvalue.value,100)
-        lvalue.value=n
+            lvalue.value+=random.randint(1,10)
         semaf.release()
             
         
 def coger_numero(Lista,Lsemaf):
-    m=111
+    m=100 #cota superior
     for i in range(NPROD):
         l=Lista[i].value
         if l < m and l!=-1: #es trampa?
             m=l
             k=i
-    return (m,k)
+    return (m,k) #devuelve tambien el productor que lo ha creado
     
         
 def producer(lvalue, semaf, Empty, Nempty):
     for v in range(N+1):
         Empty.acquire() #espera a que consuma
         dar_numero(lvalue,semaf)
+        
         print(f"producer {current_process().name} produciendo {lvalue.value} \n" )
         Nempty.release() #signal para que consuma
+    
     lvalue.value=-1 #deja de producir 
         
     
@@ -50,12 +51,16 @@ def producer(lvalue, semaf, Empty, Nempty):
 def consumer(Lista, Lsemaf, Empty, Nempty, result):   
     for i in range(NPROD):
         Nempty[i].acquire() #espero a que produzcan
+    
     for v in range(N*NPROD):
         (d,j)=coger_numero(Lista,Lsemaf)
-        result.append(d)
+        result.append((d,j)) 
+        
         print(result)
+        
         Empty[j].release() #signal para que produzca
         Nempty[j].acquire() #espero a que produzca
+    
     return result
     
     
@@ -64,12 +69,15 @@ def consumer(Lista, Lsemaf, Empty, Nempty, result):
 def main():
     values=[Value('i',-2) for i in range(NPROD)]
     Lsemaf=[Lock() for i in range(NPROD)]
+    
     non_empty = [Semaphore(0) for i in range(NPROD)] 
     #non_empty ¿hay productos en producer?
+    
     empty = [Lock() for i in range(NPROD)] 
     #empty: ¿esta vacio cada productor?
     
-    result=[]
+    manager=Manager()
+    result=manager.list()
   
     prodlst = [Process(target=producer,
                         name=f'prod_{i}',
