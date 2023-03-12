732fb5fedcf7ae353fd0fe71b1011512166d5639
crishe07 <124082370+crishe07@users.noreply.github.com>
2023-03-12 19:46:59 +0100

    Ultimos correcciones

--- p1_buffer.py
+++ p1_buffer.py
@@ -7,7 +7,7 @@ Created on Thu Feb 23 09:14:09 2023
 """
 
 from multiprocessing import Process, current_process
-from multiprocessing import Semaphore, Lock
+from multiprocessing import Semaphore, Lock, BoundedSemaphore
 from multiprocessing import Value, Manager, Array
 import random
 import math
@@ -15,61 +15,76 @@ import math
 
 NPROD = 3 #numero de productores
 LPROD= 5 #capacidad del array de los productores
-N=15 #capacidad de los productores
+N=8#capacidad de los productores
  
 
-def dar_numero(array,semaf):
-    assert array[0]!=-1
+def dar_numero(array, index, semaf):
     semaf.acquire()
-    if array[0] == -2:
-        array[0]=random.randint(0,5) #inicilización
-        print("inicio", array[0])
-    else:
-        for i in range(1,len(array)):
-            if array[i]==-2:
-                array[i]=array[i-1] +random.randint(1,20) #creciente
-                print("produciendo", array[i])
-                break
-    semaf.release()
-
+    try:
+        if index.value==0: #inicilización
+            array[index.value]=random.randint(0,5) 
+        else:
+            array[index.value]=array[index.value-1] +random.randint(1,10) #creciente
+        print(f"generado {array[index.value]} desde productor {current_process().name} \n")
+        index.value+=1
+    finally:
+        semaf.release()
         
-def coger_numero(ListaA,Lsemaf):
+
+def avanzar(array,index, semaf):
+    semaf.acquire()
+    try:
+        for i in range(len(array)-1):
+            if array[i] !=-2:
+                array[i]=array[i+1]
+    
+        array[-1]=-2
+        index.value-=1
+    finally:
+        semaf.release()
+        
+def coger_numero(ListaA,indexs, Lsemaf):
     m = math.inf
     for i in range(NPROD):
         l=ListaA[i][0]
         if l < m and l!=-1: 
             m=l
             k=i
-         #avanzamos ListaA[k]
-        for j in range(LPROD-1):
-            ListaA[k][j]=ListaA[k][j+1]
-        ListaA[k][LPROD-1]=-2
+    
+    print(f"avanzando array {k} \n")
+    
+    avanzar(ListaA[k],indexs[k],Lsemaf[k])
+    
     return (m,k) #devuelve tambien el productor que lo ha creado
     
+
         
-def producer(lvalue, semaf, Empty, Nempty):
+def producer(aray, index, semaf, Empty, Nempty):
     for v in range(N):
-        Empty.acquire() #espera a que consuma
-        dar_numero(lvalue,semaf)
+        Empty.acquire() #espera a que consuma 
         
-        print(f"producer {current_process().name} produciendo {lvalue[0]} vuelta {v+1} \n" )
+        dar_numero(aray,index,semaf)
+                
         Nempty.release() #signal para que consuma
+        
     
     Empty.acquire() 
-    
-    lvalue[0]=-1 #deja de producir 
+    aray[index.value]=-1 #deja de producir 
     Nempty.release() 
     
     
-def consumer(Lista, Lsemaf, Empty, Nempty, result):   
+def consumer(ListaA, index, Lsemaf, Empty, Nempty, result):   
     for i in range(NPROD):
+        
         Nempty[i].acquire() #espero a que produzcan
     
-    while estan_produciendo(Lista):
+    while estan_produciendo(ListaA):
         
-        (d,j)=coger_numero(Lista,Lsemaf)
+        (d,j)=coger_numero(ListaA, index, Lsemaf)
+        
         
         result.append((d, f"prod {j}"))
+        
         
         Empty[j].release() #signal para que produzca
         
@@ -78,47 +93,41 @@ def consumer(Lista, Lsemaf, Empty, Nempty, result):
     return result
     
 
-def estan_produciendo(Lista):
+def estan_produciendo(ListaA): 
     b=False
-    for i in Lista:
-        if i[0]!=-1:
+    for alm in ListaA:
+        if alm[0]!=-1:
             b=True
     return b
         
 
 def main():
-    #values=[Value('i',-2) for i in range(LPROD)]
     
-    Lsemaf=[Lock() for i in range(NPROD)]
-   
+    Lsemaf=[Lock() for i in range(NPROD)] #semaforo individual de cada productor
 
-    non_empty = [Semaphore(0) for i in range(NPROD)] #sem(0)
-    #non_empty ¿hay productos en producer?
+    non_empty = [Semaphore(0) for i in range(NPROD)]  #non_empty ¿hay productos en producer?
    
-    empty = [Lock() for i in range(NPROD)] #sem(1)
-    #empty: ¿esta vacio cada productor?
+    empty = [BoundedSemaphore(LPROD) for i in range(NPROD)]  #empty: ¿esta vacio cada productor?
     
-    almacen=Array('i',LPROD)
-    for i in range(LPROD):
-        almacen[i]=-2
+       
+    Alm=[Array('i',LPROD) for i in range(NPROD)] #un array para cada productor
+    for almacen in Alm:
+        for i in range(LPROD):
+            almacen[i]=-2
     
-    Alm=[almacen for i in range(NPROD)]
-    
-    #Añadir lista de indices
-    
-    
+    indices=[Value('i',0) for i in range(NPROD)] #indice que indica la ultima posicion del buffer donde hay un elemento
     
     manager=Manager()
-    result=manager.list()
+    result=manager.list() #lista resultado, donde el consumidor va poniendo los elementos ordenados
     
     prodlst = [Process(target=producer,
                         name=f'prod_{i}',
-                        args=(Alm[i], Lsemaf[i], empty[i], non_empty[i]))
+                        args=(Alm[i], indices[i], Lsemaf[i], empty[i], non_empty[i]))
                 for i in range(NPROD) ]
 
     consum= Process(target=consumer,
                     name= "consumidor", 
-                    args=(Alm, Lsemaf, empty, non_empty, result))
+                    args=(Alm, indices, Lsemaf, empty, non_empty, result))
     
     
     for p in prodlst:
